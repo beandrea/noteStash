5ac9ae2b20cd94dc98849333e106696b7c26b01d
Andrea <arkflame99@gmail.com>
2021-02-21 14:25:20 -0500

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,22 @@
 const router = require("expresss").Router();
 const store = require("../database/storeNote");
+
+router.get("/notes", (req, res) => {
+    store.getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+    store.addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", (req, res) => {
+    store.remove(req.params.id)
+        .then(() => res.json({ ok: true}))
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
