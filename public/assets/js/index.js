2b16fa9c9ea04889d939db186692f3c457bf7dea
Andrea <arkflame99@gmail.com>
2021-02-21 14:11:10 -0500

    index methods pt2

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -43,3 +43,89 @@ const renderActive = () => {
         $noteText.val("");
     }
 };
+
+const handleSave = function() {
+    const newN = {
+        title: title.val(),
+        text: text.val(),
+    };
+
+    saveNote(newN).then(() => {
+        getAndRender();
+        renderActive();
+    });
+};
+
+const handleDelete = function(e) {
+    e.stopPropagation();
+
+    const note = $(this).parent(".list-group-item").data();
+
+    if(active.id === note) {
+        active = {};
+    }
+
+    deleteNote(note.id).then(() => {
+        getAndRender();
+        renderActive();
+    });
+};
+
+const handleNoteView = function() {
+    active = $(this).data();
+    renderActive();
+};
+
+const handleNewNoteView = function() {
+    active = {};
+    renderActive();
+}
+
+const handleRenderSaveBtn = function() {
+    if(!title.val().trim() || !text.val().trim()) {
+        saveBtn.hide();
+    } else {
+        saveBtn.show();
+    }
+};
+
+const renderNoteLst = (notes) => {
+    noteLst.empty();
+    const noteLstItems = [];
+    const createLi = (text, withDeleteButton = true) => {
+        const li = $("<li class='list-group-item'>");
+        const span = $("<span>").text(text);
+        li.append(span);
+
+        if(withDeleteButton) {
+            const delBtn = $("<i class='fas fa-trash-alt float-right text-danger delete-note'>");
+            li.append(delBtn);
+        }
+
+        return li;
+    };
+
+    if(notes.length === 0) {
+        noteLstItems.push(createLi("No saved notes", false));
+    }
+
+    notes.forEach((note) => {
+        const li = createLi(note.title).data(note);
+        noteLstItems.push(li);
+    });
+
+    noteLst.append(noteLstItems);
+};
+
+const getAndRender = () => {
+    return getNotes().then(renderNoteLst);
+};
+
+saveBtn.on("click", handleSave);
+noteLst.on("click", ".list-group-item", handleNoteView);
+newBtn.on("click", handleNewNoteView);
+noteLst.on("click", ".delete-note", handleDelete);
+title.on("keyup", handleRenderSaveBtn);
+text.on("keyup", handleRenderSaveBtn);
+
+getAndRender();
